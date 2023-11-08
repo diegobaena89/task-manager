2f77a12a4f42edeba98efe8a8c376c738a82798e
Diego Fronteira <diego.fronteira@ab-inbev.com>
2023-11-08 18:43:39 -0300

    feat: Error handling

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
@@ -12,6 +16,15 @@ export class UserRepository extends Repository<User> {
   async createUser(authCredentialsDTO: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDTO;
     const user = this.create({ username, password });
-    await this.save(user);
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
